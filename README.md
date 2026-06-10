d36dc94dadbde396da060129160ec2591c7f260b
Apan Trikha <apan007@live.co.uk>
2026-06-10 17:22:58 +0530

    Implemented shape tracker for tensor library

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# atml
-Apan Trikha's Machine Learning stack
+# Apan Trikha's ML stack (ATML)
+
+I am making this ML stack because I couldn't find a framework that can do the
+following:
+
+- **Must be lightweight, to implement language models on a PC.** Most industrial
+grade frameworks while powerful, are bloated.
+- **Must work with multiple accelerators.** The lightweight C++ frameworks tend
+to be CPU only.
+- **Must be flexible to rapidly prototype novel models in modern C++.** C++ is
+controversial for a variety of reasons. Contrary to one of the most popular
+opinion, you can write high level code in modern C++.
+
+The core aim of the library is to **provide efficient, high level abstractions
+to infer deep learning models in modern C++.** This includes both researchers
+prototyping novel models, and developers supporting novel accelerators.
+
+## Dependencies
+- Any C++ compiler supporting C++23. GCC and Clang will work out of the box.
+
+## Current state
+Currently, this is work in progress. And I've just implemented the foundation
+to implement tensors.
+
+## Roadmap
+
+- [] Tensor library
+- [] Lazy evaluator for tensor ops
+- [] Reverse-mode autodiff
+- [] Kernel scheduling and fusion
+- [] CPU codegen support
+- [] Vulkan codegen support
+- [] ROCm codegen support
---
+++ tests/test_shape_tracker.cpp
@@ -0,0 +1,119 @@
+#include <boost/ut.hpp>
+
+#include "shape_tracker.hpp"
+
+namespace ut = boost::ut;
+
+static ut::suite shape_tracker_suite = [] {
+  using namespace ut;
+  using namespace atml;
+
+  "row_major_strides and numel"_test = [] {
+     expect(row_major_strides({2, 3, 4}) == Strides{12, 4, 1});
+
+     View view = {
+       .shape = {2, 3, 4},
+       .strides = row_major_strides({2, 3, 4}),
+       .offset = 0,
+       .mask = std::nullopt,
+     };
+
+     expect(view.numel() == 24);
+     expect(view.contiguous());
+  };
+
+  "flat offset, indexing and broadcast"_test = [] {
+    View view = {
+      .shape = {2, 3},
+      .strides = row_major_strides({2, 3}),
+      .offset = 0,
+      .mask = std::nullopt,
+    };
+
+    expect(flat_offset(view, std::array<dim_t, 2>{1, 2}) == 5);
+
+    view.offset = 10;
+    expect(flat_offset(view, std::array<dim_t, 2>{0, 0}) == 10);
+
+    View bc = {
+      .shape = {4, 3},
+      .strides = {0, 1},
+      .offset = 0,
+      .mask = std::nullopt,
+    };
+
+    expect(flat_offset(bc, std::array<dim_t, 2>{0, 2}) ==
+        flat_offset(bc, std::array<dim_t, 2>{3, 2}));
+  };
+
+  "permutation reordering"_test = [] {
+    View view = {
+      .shape = {2, 3},
+      .strides = row_major_strides({2, 3}),
+      .offset = 0,
+      .mask = std::nullopt,
+    };
+
+    View perm = permute(view, std::array<dim_t, 2>{1, 0});
+
+    expect(perm.shape == Shape{3, 2});
+    expect(perm.strides == Strides{1, 3});
+    expect(not perm.contiguous());
+
+    expect(flat_offset(perm, std::array<dim_t, 2>{2, 1}) 
+        == flat_offset(view, std::array<dim_t, 2>{1, 2}));
+  };
+
+  "expand sets stride 0 in 1D tensor"_test = [] {
+    View view = {
+      .shape = {1, 3},
+      .strides = row_major_strides({1, 3}),
+      .offset = 0,
+      .mask = std::nullopt,
+    };
+
+    View expanded = expand(view, Shape{4, 3});
+
+    expect(expanded.shape == Shape{4, 3});
+    expect(expanded.strides[0] == 0);
+    expect(expanded.strides[1] == 1);
+  };
+
+  "reshape sanity"_test = [] {
+    // Collapse on contiguous, fail in non-continguous
+    View view = {
+      .shape = {2, 3},
+      .strides = row_major_strides({2, 3}),
+      .offset = 0,
+      .mask = std::nullopt,
+    };
+
+    auto res = reshape(view, {6});
+    expect(res.has_value()) >> fatal;
+
+    expect(res->strides == Strides{1});
+
+    View perm = permute(view, std::array<dim_t, 2>{1, 0});
+    expect(not reshape(perm, {6}).has_value());
+  };
+
+  "shape tracker basics"_test = [] {
+    ShapeTracker tracker = {
+      .views = {
+        View {
+          .shape = {2, 3},
+          .strides = row_major_strides({2, 3}),
+          .offset = 0,
+          .mask = std::nullopt,
+        },
+      }
+    };
+
+    expect(tracker.shape() == Shape{2, 3});
+    expect(tracker.numel() == 6);
+    expect(tracker.contiguous());
+
+    tracker.views.push_back(tracker.top());
+    expect(not tracker.contiguous());
+  };
+};
